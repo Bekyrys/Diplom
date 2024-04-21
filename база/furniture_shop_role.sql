8a472076919d14940db0cdc02cb93383bc75deda
Bekyrys <isaev.bekos@gmail.com>
2024-04-21 14:05:20 +0500

    first commit

---
+++ база/furniture_shop_role.sql
@@ -0,0 +1,2 @@
+INSERT INTO furniture_shop.role (id, role) VALUES (1, 'ADMIN');
+INSERT INTO furniture_shop.role (id, role) VALUES (2, 'USER');
---
+++ база/furniture_shop_user.sql
@@ -0,0 +1,4 @@
+INSERT INTO furniture_shop.user (id, active, email, name, password, role_id) VALUES (8, 1, 'vitaliy@gmail.com', 'VITALITY', '$2a$10$vjJ6JJC7Yab8orxElp8yPOeRJ3ZvPWh.mkmmp0YyFmwTYm/7tq4Uy', 2);
+INSERT INTO furniture_shop.user (id, active, email, name, password, role_id) VALUES (10, 1, '22', 'admin1', '$2a$10$AtsEMgMBZmR4.UJUmu3AEedSCcjmwp6D0c0.e0qLF1IwW2cjsS7M.', 1);
+INSERT INTO furniture_shop.user (id, active, email, name, password, role_id) VALUES (18, 1, 'name@gmail.com', 'name', '$2a$10$d09e4E.CzS50kTcHEpn/cu8DAuAbFQBGasmxkNoT4dTfCgF03QaIq', 2);
+INSERT INTO furniture_shop.user (id, active, email, name, password, role_id) VALUES (19, 1, 'admin@gmail.com', 'admin', '$2a$10$0f5XYVBIvq5UCF0qeXrKy.hfFRtJx68uWtJdoca48uADrVaeEaOvO', 1);
